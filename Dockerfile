ff009207a752697ddd7135117b9f2fcd3143606e
Dennis Tang <dennisvtang@gmail.com>
2023-07-14 20:52:30 -0700

    Refactor method of modifying server.ini
    Instead of hard coding environment variables this method should allow modification of any server.ini setting thats in default_settings.txt

--- Dockerfile
+++ Dockerfile
@@ -4,43 +4,9 @@ FROM ubuntu:22.04
 ENV STEAMPORT1="8766" \
     STEAMPORT2="8767" \
     SERVER_NAME="server" \
-    SERVER_PASSWORD="" \
     SERVER_ADMIN_PASSWORD="pzadmin" \
-    SERVER_PORT="16261" \
+    DefaultPort="16261" \
     SERVER_BRANCH="" \
-    SERVER_PUBLIC="false" \
-    SERVER_PUBLIC_NAME="Project Zomboid Docker Server" \
-    SERVER_PUBLIC_DESC="" \
-    SERVER_MAX_PLAYER="16" \
-    RCON_PORT="27015" \
-    RCON_PASSWORD="" \
-    MODS="" \
-    WORKSHOP_ITEMS="" \
-    ANTICHEAT_PROTECTION_TYPE1="true" \
-    ANTICHEAT_PROTECTION_TYPE2="true" \
-    ANTICHEAT_PROTECTION_TYPE3="true" \
-    ANTICHEAT_PROTECTION_TYPE4="true" \
-    ANTICHEAT_PROTECTION_TYPE5="true" \
-    ANTICHEAT_PROTECTION_TYPE6="true" \
-    ANTICHEAT_PROTECTION_TYPE7="true" \
-    ANTICHEAT_PROTECTION_TYPE8="true" \
-    ANTICHEAT_PROTECTION_TYPE9="true" \
-    ANTICHEAT_PROTECTION_TYPE10="true" \
-    ANTICHEAT_PROTECTION_TYPE11="true" \
-    ANTICHEAT_PROTECTION_TYPE12="true" \
-    ANTICHEAT_PROTECTION_TYPE13="true" \
-    ANTICHEAT_PROTECTION_TYPE14="true" \
-    ANTICHEAT_PROTECTION_TYPE15="true" \
-    ANTICHEAT_PROTECTION_TYPE16="true" \
-    ANTICHEAT_PROTECTION_TYPE17="true" \
-    ANTICHEAT_PROTECTION_TYPE18="true" \
-    ANTICHEAT_PROTECTION_TYPE19="true" \
-    ANTICHEAT_PROTECTION_TYPE20="true" \
-    ANTICHEAT_PROTECTION_TYPE21="true" \
-    ANTICHEAT_PROTECTION_TYPE22="true" \
-    ANTICHEAT_PROTECTION_TYPE23="true" \
-    ANTICHEAT_PROTECTION_TYPE24="true" \
-    SPAWN_POINT="0,0,0" \
 	UID="1000" \
 	GID="1000"
 
@@ -60,13 +26,13 @@ RUN useradd -u ${UID} -U -m -s /bin/false pzombie && usermod -G users pzombie
 # Expose ports
 EXPOSE $STEAMPORT1/udp
 EXPOSE $STEAMPORT2/udp
-EXPOSE $SERVER_PORT/udp
+EXPOSE $DefaultPort/udp
 EXPOSE ${RCON_PORT}
-
-VOLUME ["/data/server-file", "/data/config"]
 
 # add default spawn locations from hosting server through steam
 COPY server_spawnregions.lua /data/config/Server/server_spawnregions.lua
 
-COPY entry.sh /data/scripts/entry.sh
-CMD ["bash", "/data/scripts/entry.sh"]
+COPY /res/default_settings.txt /scripts/default_settings.txt
+
+COPY entry.sh /scripts/entry.sh
+CMD ["bash", "/scripts/entry.sh"]
--- entry.sh
+++ entry.sh
@@ -45,41 +45,23 @@ echo "Updating ${SERVER_NAME}.ini..."
 mkdir -p /data/config/Server
 touch ${server_ini}
 
-echo "DefaultPort=${SERVER_PORT}" >> ${server_ini}
-echo "Password=${SERVER_PASSWORD}" >> ${server_ini}
-echo "Public=${SERVER_PUBLIC}" >> ${server_ini}
-echo "PublicName=${SERVER_PUBLIC_NAME}" >> ${server_ini}
-echo "PublicDescription=${SERVER_PUBLIC_DESC}" >> ${server_ini}
-echo "RCONPort=${RCON_PORT}" >> ${server_ini}
-echo "RCONPassword=${RCON_PASSWORD}" >> ${server_ini}
-echo "MaxPlayers=${SERVER_MAX_PLAYER}" >> ${server_ini}
-echo "Mods=${MODS}" >> ${server_ini}
-echo "WorkshopItems=${WORKSHOP_ITEMS}" >> ${server_ini}
-echo "AntiCheatProtectionType1=${ANTICHEAT_PROTECTION_TYPE1}" >> ${server_ini}
-echo "AntiCheatProtectionType2=${ANTICHEAT_PROTECTION_TYPE2}" >> ${server_ini}
-echo "AntiCheatProtectionType3=${ANTICHEAT_PROTECTION_TYPE3}" >> ${server_ini}
-echo "AntiCheatProtectionType4=${ANTICHEAT_PROTECTION_TYPE4}" >> ${server_ini}
-echo "AntiCheatProtectionType5=${ANTICHEAT_PROTECTION_TYPE5}" >> ${server_ini}
-echo "AntiCheatProtectionType6=${ANTICHEAT_PROTECTION_TYPE6}" >> ${server_ini}
-echo "AntiCheatProtectionType7=${ANTICHEAT_PROTECTION_TYPE7}" >> ${server_ini}
-echo "AntiCheatProtectionType8=${ANTICHEAT_PROTECTION_TYPE8}" >> ${server_ini}
-echo "AntiCheatProtectionType9=${ANTICHEAT_PROTECTION_TYPE9}" >> ${server_ini}
-echo "AntiCheatProtectionType10=${ANTICHEAT_PROTECTION_TYPE10}" >> ${server_ini}
-echo "AntiCheatProtectionType11=${ANTICHEAT_PROTECTION_TYPE11}" >> ${server_ini}
-echo "AntiCheatProtectionType12=${ANTICHEAT_PROTECTION_TYPE12}" >> ${server_ini}
-echo "AntiCheatProtectionType13=${ANTICHEAT_PROTECTION_TYPE13}" >> ${server_ini}
-echo "AntiCheatProtectionType14=${ANTICHEAT_PROTECTION_TYPE14}" >> ${server_ini}
-echo "AntiCheatProtectionType15=${ANTICHEAT_PROTECTION_TYPE15}" >> ${server_ini}
-echo "AntiCheatProtectionType16=${ANTICHEAT_PROTECTION_TYPE16}" >> ${server_ini}
-echo "AntiCheatProtectionType17=${ANTICHEAT_PROTECTION_TYPE17}" >> ${server_ini}
-echo "AntiCheatProtectionType18=${ANTICHEAT_PROTECTION_TYPE18}" >> ${server_ini}
-echo "AntiCheatProtectionType19=${ANTICHEAT_PROTECTION_TYPE19}" >> ${server_ini}
-echo "AntiCheatProtectionType20=${ANTICHEAT_PROTECTION_TYPE20}" >> ${server_ini}
-echo "AntiCheatProtectionType21=${ANTICHEAT_PROTECTION_TYPE21}" >> ${server_ini}
-echo "AntiCheatProtectionType22=${ANTICHEAT_PROTECTION_TYPE22}" >> ${server_ini}
-echo "AntiCheatProtectionType23=${ANTICHEAT_PROTECTION_TYPE23}" >> ${server_ini}
-echo "AntiCheatProtectionType24=${ANTICHEAT_PROTECTION_TYPE24}" >> ${server_ini}
-echo "SpawnPoint=${SPAWN_POINT}" >> ${server_ini}
+default_settings_path="/scripts/default_settings.txt"
+while read line; do
+  # split setting name and default value
+  IFS='=' read -r -a setting_pair <<< "$line"
+  setting_name="${setting_pair[0]}"
+  setting_default_value="${setting_pair[1]}"
+
+  # check if there isn't a variable with matching setting name
+  if [ -z "${!setting_name}" ]
+  then
+    # use default value
+    echo "${setting_name}=${setting_default_value}" >> $server_ini
+  else
+    # use docker compose environment variable value
+    echo "${setting_name}=${!setting_name}" >> $server_ini
+  fi
+done < $default_settings_path
 
 chown -R pzombie:pzombie /data/config/
 
